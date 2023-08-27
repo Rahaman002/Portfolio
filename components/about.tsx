6c3247fc3930f5c7d5060e2d9eb985e2e8890395
Rahaman <36rahaman@gmail.com>
2023-08-27 19:17:16 +0530

    final touch

--- components/about.tsx
+++ components/about.tsx
@@ -19,30 +19,7 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-        After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{" "}
-        <span className="font-medium">full-stack web development</span>.{" "}
-        <span className="italic">My favorite part of programming</span> is the
-        problem-solving aspect. I <span className="underline">love</span> the
-        feeling of finally figuring out a solution to a problem. My core stack
-        is{" "}
-        <span className="font-medium">
-          React, Next.js, Node.js, and MongoDB
-        </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
-        learn new technologies. I am currently looking for a{" "}
-        <span className="font-medium">full-time position</span> as a software
-        developer.
-      </p>
-
-      <p>
-        <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{" "}
-        <span className="font-medium">learning new things</span>. I am currently
-        learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
+      I am Abdul Rahaman, a highly motivated B.Tech CSE graduate with a strong foundation in programming languages like C, C++, and Python, complemented by web development skills in HTML, CSS, JavaScript, and React. My internship experience at Topnotch Solutions solidified my proficiency in web development technologies, while my involvement in cutting-edge projects like hand gesture-based virtual mouse control and deep learning-based medical image analysis demonstrates my commitment to innovation. I am a published author and a dedicated learner, constantly seeking self-improvement and new technological challenges. My blend of academic excellence, problem-solving abilities, and leadership skills make me an asset to any tech-driven organization looking for a dynamic and driven team member.
       </p>
     </motion.section>
   );
